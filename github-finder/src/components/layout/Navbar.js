887bbe5136ccaa50694563c28f8c62ac3ac29069
Simran Sandhu <7simransandhu@gmail.com>
2020-08-20 01:11:58 -0400

    changed Navbar, UserItem to fxn based
    also cleaned up some props by making function params specific props and not just writing "prop"

--- github-finder/src/components/layout/Navbar.js
+++ github-finder/src/components/layout/Navbar.js
@@ -1,32 +1,25 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
+// Instead of using props as the argument, just write the specific props
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon}></i> {title}
+      </h1>
+    </nav>
+  );
+};
 
+Navbar.defaultProps = {
+  title: 'Insert Title',
+  icon: 'fas fa-question-circle',
+};
 
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
-
-export class Navbar extends Component {
-    // Default props if we do not add any
-    static defaultProps = {
-        title: 'Insert Title',
-        icon: 'fas fa-question-circle'
-    }
- 
-
-    // Makes sure that our Prop's are of the correct type(string, const... etc)
-    static propTypes = {
-        title:PropTypes.string.isRequired,
-        icon:PropTypes.string.isRequired
-    }
-    render() {
-        return (
-            <nav className="navbar bg-primary">
-                <h1>
-                    <i className={this.props.icon}></i> {this.props.title}
-                </h1>
-            </nav>
-        )
-    }
-}
-
-export default Navbar
+export default Navbar;
--- github-finder/src/components/users/UserItem.js
+++ github-finder/src/components/users/UserItem.js
@@ -1,51 +1,31 @@
 // Used 'rce' to generate class based component
 
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  // Constructor is not manditory now
+// Props are now passed as an argument
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
 
-  // constructor(){
-  //     // super is required in classes
-  //     super();
-  //     this.state = {
-  //         id: 'id',
-  //         login: 'mojombo',
-  //         avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-  //         html_url: "https://github.com/mojombo"
-  //     }
-  // }
-
-  // state = {
-  //     id: 'id',
-  //     login: 'mojombo',
-  //     avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-  //     html_url: "https://github.com/mojombo"
-  // }
-
-  render() {
-    // now we dont need to write this.state.___
-
-    // we will now be using props.user to get this info
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired, //ptor
+};
 
 export default UserItem;
