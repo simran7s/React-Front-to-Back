8d7d95ee74b605365e16a680934154f48fbf6b11
Simran Sandhu <7simransandhu@gmail.com>
2020-08-20 01:29:50 -0400

    Add axios to App.js to get users
    Used Axios. Also removed hardcoded state and are now using Axios to get data from Github API to use as our data

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 import Users from './components/users/Users';
 import Navbar from './components/layout/Navbar';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  // Runs when app component mounts(on load)
+  async componentDidMount() {
+    // While fetching data, setState of loading to true(for loading wheel to be called)
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    // After data is recieved, setState of users to data recieve and turn off the loading wheel
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className="App">
         {/* We just gave props title, icon to the navbar component */}
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
-          <Users />
+          {/* We now pass our state as props to Users component */}
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- github-finder/src/components/users/Users.js
+++ github-finder/src/components/users/Users.js
@@ -2,32 +2,11 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        login: 'mojombo',
-        id: '1',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        login: 'defunkt',
-        id: '2',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        login: 'pjhyett',
-        id: 3,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-    ],
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {/* Change this.state to this.props to get state from props now */}
+        {this.props.users.map((user) => (
           // We need a key that is unique to each user when using map
           <UserItem key={user.id} user={user} />
         ))}
